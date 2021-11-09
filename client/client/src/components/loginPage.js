59e8e1c803c88eb4d4e8bec76c221d985ccc8b82
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-11-09 13:28:35 -0500

    added front end

---
+++ client/client/src/components/loginPage.js
@@ -0,0 +1,52 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import { useState } from "react";
+
+const LogInPage = () => {
+  let [password, setPassword] = useState("");
+  let [username, setUsername] = useState("");
+
+  const handlePasswordChange = (event) => {
+    event.preventDefault();
+    setPassword((password = event.target.value));
+  };
+  const handleUsernameChange = (event) => {
+    event.preventDefault();
+    setUsername((username = event.target.value));
+  };
+  const handleSubmission = () => {
+    console.log(`Form Submitted`);
+    console.log(`the password is: ${password}`);
+    console.log(`the username is: ${username}`);
+  };
+
+  return (
+    <Container className="border">
+      <Form className="m-3">
+        <h1 className="border-bottom">Log In</h1>
+        <Form.Group>
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Username"
+            onChange={handleUsernameChange}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter Password"
+            onChange={handlePasswordChange}
+          ></Form.Control>
+        </Form.Group>
+        <Button className="mt-3" onClick={handleSubmission}>
+          Log In
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export { LogInPage };
